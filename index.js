1e0e4328fa4da317ed04b22759773c54e7064d22
abhishree07 <68280108+abhishree07@users.noreply.github.com>
2021-09-12 21:07:07 +0530

    To-Do app
    Week 2 Assignment

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-let screenValue = '';
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-        else if (buttonText == 'C') {
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonText == '=') {
-            screen.value = eval(screenValue);
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
+//dependencies required for the app
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
-    })
-}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+//render css files
+app.use(express.static("public"));
 
+//placeholders for added task
+var task = ["buy socks", "practise with nodejs"];
+//placeholders for removed task
+var complete = ["finish jquery"];
+
+//post route for adding new task 
+app.post("/addtask", function(req, res) {
+    var newTask = req.body.newtask;
+    //add the new task from the post route
+    task.push(newTask);
+    res.redirect("/");
+});
+
+//render the ejs and display added task, completed task
+app.get("/", function(req, res) {
+    res.render("index", { task: task, complete: complete });
+});
+
+//set app to listen on port 3000
+app.listen(3000, function() {
+    console.log("server is running on port 3000");
+});
